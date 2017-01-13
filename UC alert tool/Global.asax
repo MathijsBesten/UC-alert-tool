74170b904d51db7a1d3603a83287674e36aeaa3f
Mathijs <m88_db@hotmail.com>
2017-01-13 13:29:51 +0100

    Add project files.

---
+++ UC alert tool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UC_alert_tool.MvcApplication" Language="C#" %>
---
+++ UC alert tool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UC_alert_tool.Startup))]
+namespace UC_alert_tool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
