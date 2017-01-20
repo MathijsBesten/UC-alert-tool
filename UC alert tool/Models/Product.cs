032c7a955ab6e5109c2d655d10a28ae3c8dc50c4
Mathijs <m88_db@hotmail.com>
2017-01-20 11:15:04 +0100

    Added storing and Product model

---
+++ UC alert tool/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UC_alert_tool.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string naam { get; set; }
+        public int helpdeskID { get; set; }
+    }
+}
---
+++ UC alert tool/Models/Storing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UC_alert_tool.Models
+{
+    public class Storing
+    {
+        public int ID { get; set; }
+        public int productID { get; set; }
+        public string Titel { get; set; }
+        public int eigenaarID { get; set; }
+        public DateTime begindatum { get; set; }
+        public DateTime einddatum { get; set; }
+        public bool isGesloten { get; set; }
+    }
+}
