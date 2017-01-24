f2b070fad82eec54169b2322abc3ac221be6c5c6
Mathijs <m88_db@hotmail.com>
2017-01-24 11:56:18 +0100

    added a check if user is logged in order to show the edit button on news item added custom attribute file for later use

---
+++ UC alert tool/Attributes/RedirectToHomepageIfNotAuthorizeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UC_alert_tool.Attributes
+{
+    public class RedirectToHomepageIfNotAuthorizeAttribute : AuthorizeAttribute
+    {
+
+    }
+}
