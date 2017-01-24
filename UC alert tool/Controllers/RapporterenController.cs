391c0659cc5147a422ef70497aadb2594d51c552
Mathijs <m88_db@hotmail.com>
2017-01-24 16:21:37 +0100

    added rapporteren controller for the reporting issues

---
+++ UC alert tool/Controllers/RapporterenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UC_alert_tool.Controllers
+{
+    public class RapporterenController : Controller
+    {
+        // GET: Rapporteren
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
