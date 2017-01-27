f31e20090914997d3888918bf97a082fd27f7dcf
Mathijs <m88_db@hotmail.com>
2017-01-27 15:41:20 +0100

    added email model and contoller ( the email model can be changed  at a later date) The email function is working but only for one recipient. "Multiple recipients" support will be added at a later date

---
+++ UC alert tool/Functions/Email/Sending.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+using UC_alert_tool.Models;
+
+namespace UC_alert_tool.Functions.Email
+{
+    public class Sending
+    {
+        public static void sendEmailToOne(string fromEmailAddress, string toEmailAddress, string smtpServerAddress, string username, string password, string subject, string body)
+        {
+            MailMessage mail = new MailMessage(fromEmailAddress, toEmailAddress, subject, body);
+            SmtpClient smtpClient = new SmtpClient(smtpServerAddress);
+            smtpClient.Credentials = new NetworkCredential(username, password);
+            smtpClient.EnableSsl = true;
+            try
+            {
+                smtpClient.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex); ;
+            }
+        }
+    }
+}
---
+++ UC alert tool/Models/EmailViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UC_alert_tool.Models
+{
+    public class email
+    {
+        public string FromEmailAddress { get; set; }
+        public string SMTPServerURL { get; set; }
+        public string SMTPUsername { get; set; }
+        public string SMTPPassword { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailBody { get; set; }
+    }
+
+    public class emailToMany : email
+    {
+        public List<string> Recipients { get; set; }
+    }
+
+    public class emailToOne : email
+    {
+        public string Recipient { get; set; }
+    }
+}
