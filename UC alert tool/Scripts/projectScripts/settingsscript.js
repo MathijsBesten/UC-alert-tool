1c154497c409ade9ba2ea920b0b4403bb4f2f4ae
Mathijs <m88_db@hotmail.com>
2017-02-20 14:44:19 +0100

    added a sidebar to the settings page updated bootstrap and jquery to the most recent version added a shared layout for the settings pages

---
+++ UC alert tool/Scripts/projectScripts/settingsscript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(window).bind("load resize", function () {
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-settings').addClass('collapse');
+            $('div.settingsContentWrapper').css('margin-left', '0px');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-settings').removeClass('collapse');
+            $('div.settingsContentWrapper').css('margin-left', '250px');
+
+        }
+    });
+});
