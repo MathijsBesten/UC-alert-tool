bbb5d7fdd0cf2dab9eeb564b1eebe61d3b0462af
Mathijs <m88_db@hotmail.com>
2017-02-27 14:32:12 +0100

    While making a melding, the user will get a error message when not giving a end date or a end time

--- UC alert tool/Scripts/projectScripts/generalscript.js
+++ UC alert tool/Scripts/projectScripts/generalscript.js
@@ -10,4 +10,58 @@
         }
 
     });
+    $('#submitbutton').click(checkIfstartIsBeforeEnddate);
+
+    function checkIfstartIsBeforeEnddate() {
+        $('.errortext').remove();
+        var begindate = new Date($('#begindatum').val());
+        var beginTime = $('#begintijd').val();
+        var beginTimeSplit = beginTime.split(':');
+        var enddate = new Date($('#einddatum').val());
+        var endTime = $('#eindtijd').val();
+        var endTimeSplit = endTime.split(':');
+        if (enddate != "Invalid Date") {
+            if (begindate < enddate)
+            {
+                console.log("date correct")
+                if (endTime == "") {
+                    $('#eindtijd').after("<p class='errortext'>Vul een einddatum in</p>");
+                    return false
+                }
+                else {
+                    return true;
+                }
+            }
+            else if (begindate > enddate) {
+                $('#einddatum').after("<p class='errortext'>Einddatum mag niet eerder zijn dan de begindatum</p>");
+                return false;
+            }
+            else // dates are the same
+            {
+                if (beginTime == "" || endTime == "") {
+                    $('#eindtijd').after("<p class='errortext'>Vul een einddatum in</p>");
+                    return false;
+
+                }
+                else if (beginTimeSplit[0] > endTimeSplit[0]) 
+                {
+                    $('#eindtijd').after("<p class='errortext'>Vul een correcte einddatum in</p>");
+                    return false;
+                }
+                else if (beginTimeSplit[0] == endTimeSplit[0]) {
+                    if (beginTimeSplit[1] >= endTimeSplit[1]) {
+                        $('#eindtijd').after("<p class='errortext'>De eindtijd kan niet minder of hetelfde zijn als de begintijd</p>");
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }    
+        }
+
+    };
 });
