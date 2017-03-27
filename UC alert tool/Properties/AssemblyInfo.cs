5bfb4ce0efdc10b1740c9cf43b1d4d1e42d254ef
Mathijs den Besten <m88_db@hotmail.com>
2017-03-27 15:35:32 +0200

    added a version notation for easier bug tracing

--- UC alert tool/Properties/AssemblyInfo.cs
+++ UC alert tool/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("UC_alert_tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("UC Systems")]
 [assembly: AssemblyProduct("UC_alert_tool")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("NL-nl")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
