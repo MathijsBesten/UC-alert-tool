685d5a650787ee80fe4a1ce7bf6142fe0cd0c67b
Mathijs den Besten <m88_db@hotmail.com>
2017-04-25 15:39:06 +0200

    now the program can get debiteurennummers

--- UC alert tool/Functions/Helpdesk/Get.cs
+++ UC alert tool/Functions/Helpdesk/Get.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace UC_alert_tool.Functions.Helpdesk
 {
     public class Get
     {
+        private static ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static void getAllUsers()
         {
             using (var db = new Models.supportcenterEntities())
@@ -24,6 +26,23 @@ namespace UC_alert_tool.Functions.Helpdesk
                     var userID = departmentuser.USERID;
                     var aaaUserItem = db.AaaUser.First(u => u.USER_ID == userID);
                     allContactInfo.Add(aaaUserItem.AaaContactInfo.First());
+                    try
+                    {
+                        Klanten klant = new Klanten()
+                        {
+                            PrimaireEmail = aaaUserItem.AaaContactInfo.First().EMAILID,
+                            Telefoonnummer = aaaUserItem.AaaContactInfo.First().MOBILE,
+                            debiteurnummer = aaaUserItem.AaaContactInfo.First().AaaUser.First().SDUser.Customer_Requester.First().Customer.AaaOrganization.Customer.Department_Account.First().Customer_Fields.UDF_LONG1.ToString(),
+                            Naam = aaaUserItem.AaaContactInfo.First().EMAILID
+                        };
+                        allNewContacts.Add(klant);
+                    }
+                    catch (Exception e)
+                    {
+                        log.Error("Could not add user \'" +aaaUserItem.AaaContactInfo.First().EMAILID + " \' to the database, user has probably no company assigned to him/her");
+                        log.Error("Error: " + e);
+                    }
+
                 }
                 
                 Console.Write("");
